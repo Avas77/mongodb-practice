eaef5a276bbcfb8562db0d45af3bb4589081c1db
Avas <avas.bajracharya@renegadeinsurance.com>
2023-11-22 14:46:32 +0545

    feat: added user login api

--- src/routes/user.js
+++ src/routes/user.js
@@ -64,4 +64,16 @@ router.delete("/user/:id", async (req, res) => {
   }
 });
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
 module.exports = router;
