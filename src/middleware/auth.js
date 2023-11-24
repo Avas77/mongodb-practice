407ae03dcd2e1ac2ceb98d25eeef7c447ac0b85b
Avas <avas.bajracharya@renegadeinsurance.com>
2023-11-24 09:29:08 +0545

    feat: added middleware function to authenticate apis

---
+++ src/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", ""); // get authorization token from header
+    const decoded = jwt.verify(token, "test"); // decode the token to match with the token built using our secret key
+    const user = await User.findOne({
+      //find the user in the database with the matching token
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    if (!user) {
+      throw new Error();
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send("Please authenticate");
+  }
+};
+
+module.exports = auth;
