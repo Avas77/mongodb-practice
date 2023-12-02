e6cbffba41c9088d3a7d786d425dd0a07d611d5e
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-02 20:46:39 +0545

    feat: added sorting to task router

--- src/routes/task.js
+++ src/routes/task.js
@@ -20,8 +20,13 @@ router.post("/tasks", auth, async (req, res) => {
 // get task of user
 router.get("/tasks", auth, async (req, res) => {
   const match = { owner: req.user._id };
+  const sort = {};
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
   try {
     await req.user.populate({
@@ -30,6 +35,7 @@ router.get("/tasks", auth, async (req, res) => {
       options: {
         limit: parseInt(req.query.limit),
         skip: parseInt(req.query.skip),
+        sort,
       },
     });
     res.send(req.user.tasks);
